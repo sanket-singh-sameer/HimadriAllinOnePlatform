e01e0fd0e97f468727a325be7e57df64bedb2130
sanket-singh-sameer <sanketsinghsameer6055@gmail.com>
2025-08-17 22:07:45 +0530

    learning git 2.0

--- FRONTEND/src/Pages/Home.jsx
+++ FRONTEND/src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-      <h1 data-aos="fade-up">Welcome to Himadri Boys Hostel</h1>
+      <h1 data-aos="fade-up" className='text-4xl font-bold'>Welcome to Himadri Boys Hostel</h1>
       <p>Your comfort is our priority.</p>
       <p>Your comfort is our priority.</p>
       <p>Your comfort is our priority.</p>
