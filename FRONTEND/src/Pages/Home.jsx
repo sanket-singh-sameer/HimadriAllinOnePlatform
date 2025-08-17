8935d766b2a2d6456a4bf7d397aa635ed31c8705
divyamsingh007 <legendprice007@gmail.com>
2025-08-17 22:00:52 +0530

    learnig from mistake

--- FRONTEND/src/Pages/Home.jsx
+++ FRONTEND/src/Pages/Home.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Himadri Boys Hostel</h1>
+      <h1 data-aos="fade-up">Welcome to Himadri Boys Hostel</h1>
+      <p>Your comfort is our priority.</p>
+      <p>Your comfort is our priority.</p>
+      <p>Your comfort is our priority.</p>
       <p>Your comfort is our priority.</p>
       <div>
         added by sanket.
