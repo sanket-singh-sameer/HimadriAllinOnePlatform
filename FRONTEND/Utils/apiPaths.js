e7e223a8645d66f1cc6e469caad769a176f77d95
sanket-singh-sameer <sanketsinghsameer6055@gmail.com>
2025-08-24 16:48:27 +0530

    Setup env val for apiPaths.js

--- FRONTEND/Utils/apiPaths.js
+++ FRONTEND/Utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8080/api/v1";
+export const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8080/api/v1";
 
 export const API_PATHS = {
   LOGIN: "/auth/login",
